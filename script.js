661420b0576138051e8cf4be8cac47c66c01a4e4
ericstober <erictstober@gmail.com>
2024-06-24 08:35:36 -0500

    create cards and add flip effect with js

--- script.js
+++ script.js
@@ -9,3 +9,68 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear')
 const addContainer = document.getElementById('add-container')
+
+// Keep track of current card
+let currentActiveCard = 0;
+
+// Store DOM cards
+const cardsEl = [];
+
+// Store card data
+const cardData = [
+  {
+    question: 'What must a variable begin with?',
+    answer: 'A letter, $ or _'
+  },
+  {
+    question: 'What is a variable?',
+    answer: 'Container for a piece of data'
+  },
+  {
+    question: 'Example of Case Sensitive Variable',
+    answer: 'thisIsAVariable'
+  }
+];
+
+// Create all cards
+function createCards() {
+  cardData.forEach((data, index) => createCard(data, index));
+}
+
+// Create a single card in DOM
+function createCard(data, index) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  if (index === 0) {
+    card.classList.add('active');
+  }
+
+  card.innerHTML = `
+    <div class="inner-card">
+      <div class="inner-card-front">
+        <p>${data.question}</p>
+      </div>
+
+      <div class="inner-card-back">
+        <p>${data.answer}</p>
+      </div>
+    </div>
+  `;
+
+  card.addEventListener('click', () => card.classList.toggle('show-answer'));
+
+  // Add to DOM cards
+  cardsEl.push(card);
+
+  cardsContiner.appendChild(card);
+
+  updateCurrentText();
+}
+
+// Show number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+createCards();
